48e3fdb8edc67c31ac9e65aae4d7ec21d06635f1
Luis <luisfernando000cruzdesousa@gmail.com>
2024-11-02 19:42:53 -0300

    Creating asset folder inserting all assets Creating the menu background, sound and fonts Creating the const file

--- Code/menu.py
+++ Code/menu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from asset.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, C_WHITE
 
 
 class Menu:
@@ -14,6 +18,12 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
             pygame.display.flip()
 
             # check for all events
@@ -22,3 +32,8 @@ class Menu:
                     pygame.quit()  # close window
                     quit()  # end pygame
 
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
