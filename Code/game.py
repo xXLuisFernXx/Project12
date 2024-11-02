0f9ce38f0e1436c8e0064b38c895d459a0c70fdc
Luis <luisfernando000cruzdesousa@gmail.com>
2024-11-02 20:30:06 -0300

    Save

--- Code/game.py
+++ Code/game.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+
 import pygame
 
-from Code.menu import Menu
-from asset.Const import WIN_HEIGHT, WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
+from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -9,16 +15,27 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
-
-
+    def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
 
-            # check for all events
-            #for event in pygame.event.get():
-                #if event.type == pygame.QUIT:
-                   # pygame.quit()  # close window
-                    #quit()  # end pygame
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                player_score = [0, 0]  # [Player1, Player2]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()  # Close Window
+                quit()  # end pygame
+            else:
+                pygame.quit()
+                sys.exit()
