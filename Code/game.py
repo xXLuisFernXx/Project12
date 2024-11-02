c8385a8a57eb6ddba87754aeee2f3720bd52f0f6
Luis <luisfernando000cruzdesousa@gmail.com>
2024-11-02 18:51:15 -0300

    save

--- Code/game.py
+++ Code/game.py
@@ -1,14 +1,17 @@
 import pygame
 
 from Code.menu import Menu
+from asset.Const import WIN_HEIGHT, WIN_WIDTH
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 400))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
--- Code/menu.py
+++ Code/menu.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/orig1.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            pygame.display.flip()
+
+            # check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close window
+                    quit()  # end pygame
+
